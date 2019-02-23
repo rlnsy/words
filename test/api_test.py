908d8984448133457e9e9db1b8b018499e4fffd4
rowlindsay <rowlindsay@gmail.com>
2019-02-22 23:58:42 -0800

    test file

--- test/api_test.py
+++ test/api_test.py
@@ -1,9 +1,22 @@
 import unittest
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class GetPuzzleTests(unittest.TestCase):
+    def test_successful(self):
+        url = 'http://127.0.0.1:5000/puzzles/nyt'
+        params = {
+            'day': 22,
+            'month': 2,
+            'year': 2019
+        }
+        request = Request(url, urlencode(params).encode())
+        response = urlopen(request)
+        if response.status == 200:
+            print("Get recieved a proper puzzle response")
+        else:
+            print("Something went wrong with the server")
 
 
 if __name__ == '__main__':
