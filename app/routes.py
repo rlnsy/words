5adeea70c63326353a5e609edb72abef0163229d
rowlindsay <rowlindsay@gmail.com>
2019-02-22 23:58:10 -0800

    status codes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_api import status
 import data
 from sources.Sources import format_date
+from exceptions import DatabaseError
 
 app = Flask(__name__)
 
@@ -19,9 +21,12 @@ def get_puzzle(collection):
     year = params['year']
     if (day is None) or (month is None) or (year is None):
         print("client provided invalid syntax in query")
-        return "URL syntax error"  # TODO
+        return "URL syntax error", status.HTTP_400_BAD_REQUEST
     else:
         date = format_date(day=day, month=month, year=year)
         print("valid date, fetching (%s)..." % date)
-        puzzle = data.get_puzzle(day=int(day), month=int(month), year=int(year), collection=collection)
+        try:
+            puzzle = data.get_puzzle(day=int(day), month=int(month), year=int(year), collection=collection)
+        except DatabaseError:
+            return "Something went wrong with the server :(", status.HTTP_204_NO_CONTENT
         return jsonify(puzzle)
