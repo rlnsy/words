7782d1de50e2fd8eca5f4691dff88ba8e6f32a3d
rowlindsay <rowlindsay@gmail.com>
2019-02-25 23:10:31 -0800

    basic collection view stubs

--- puzzles/collection/views.py
+++ puzzles/collection/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from sources.Sources import Sources, format_date
+from sources.exceptions import SourceError
 
 
 def index(request):
     return HttpResponse("Hello. You're at the collections index.")
+
+
+def get(request, collection_name):
+    date = format_date(24, 12, 2018)
+    try:
+        puzzle = Sources.fetch_puzzle(date, collection_name)
+    except SourceError:
+        return HttpResponse("ERROR")
+    return JsonResponse(puzzle)
